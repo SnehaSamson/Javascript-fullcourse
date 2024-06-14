59d650db9299abf82bc9e288bea34d78f1715e75
SnehaMullur <snehasidic87@gmail.com>
2024-06-15 01:19:52 +0530

    some more concepts

---
+++ WalkingTheDOM.js
@@ -0,0 +1,15 @@
+// Accessing children of an element 
+
+// console.log(document.body.firstChild);
+// console.log(document.body.lastChild);
+// console.log(document.body.childNodes);
+
+// console.log(document.body.firstChild);
+// a = document.body.firstChild;
+// console.log(a.parentNode);
+// console.log(a.parentElement);
+// console.log(a.firstChild.nextSibling);
+
+let b = document.body
+console.log("First child of b is : ",b.firstChild);
+console.log("First Element of b is : ",b.firstElementChild);
